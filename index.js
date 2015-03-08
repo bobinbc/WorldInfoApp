a546d8bb5673a33b944584e4e28e2bc80ba23527
Bobin Cherian <bobinbc@gmail.com>
2015-03-07 23:35:14 -0500

    Initial Weather

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var http = require('http');
-
+var isFirstTime = true;
 var app = express();
 var port = process.env.PORT || 5000;
+var router = express.Router();
 
 http.createServer(app).listen(port, function(){
-    //console.log("Listening on Port %d", port);
-})
+    console.log("Listening on Port %d", port);
+});
 
 
 
@@ -21,4 +22,41 @@ http.createServer(app).listen(port, function(){
 //    res.end(req.params.id + " rocks!");
 //});
 
-app.use(express.static(__dirname + '/www/'));
+// app.use(express.static(__dirname + '/www/'));
+
+// app.use(express.static(__dirname + '/www/'));
+ app.use(express.static(__dirname + '/www/'));
+ app.use('/',router);
+router.use(function(req,res,next){	  
+	  console.log('inCreate Function before');
+	  if(isFirstTime === true)
+	  {
+	  setTimeout(function(){
+		   console.log('increate timeout function');
+		   // res.write(200);
+		   res.send('www.google.com');
+		   // res.end();
+		   isFirstTime = false;
+	   },5000);
+	  }
+	  else
+	  {
+		  console.log('Again...');
+	  }
+	  next();
+  });
+ 
+
+// app.all("*",function(req,res){
+	// setTimeout(function(){
+		 // console.log('increate timeout function');
+	 // },10000);
+	// // http.redirect('/','main/index.html');
+	
+	// console.log('inCreate Function');
+// });
+	 
+
+
+	 
+	 
---
+++ www/js/controllers.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+var controllers = angular.module('worldInfo.controllers', []);
+
+
+controllers.controller('AppCtrl', function ($scope) {
+    $scope.name = "Module!";
+
+});
+
+
+controllers.controller('GetCityNameCtrl', function ($scope, $http) {
+//    $scope.data;
+//    $scope.city = "London";
+
+    $scope.updateCity = function ($scope, $http) {
+//        this.city = "Kitchener";
+//        $scope.data;
+
+        $http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=London,uk&&units=metric&&callback=JSON_CALLBACK').then(function (data) {
+            $scope.data = data.data;
+            console.log(data);
+        })
+
+    };
+});
+
+controllers.controller('WeatherListCtrl', function ($scope, $http) {
+    $scope.data;
+//$scope.city = "London";
+    $http.jsonp('http://api.openweathermap.org/data/2.5/weather?q=Kitchener&&units=metric&&callback=JSON_CALLBACK').then(function (data) {
+        $scope.data = data.data;
+        console.log(data);
+    })
+});
